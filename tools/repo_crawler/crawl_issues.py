8a341c46f5efe8826051c889a00c2cdbc01aacf6
Jinglei Ren <jinglei@persper.org>
2017-03-25 21:07:29 +0800

    [tools] Support more PR detection and write invalid issue files

--- tools/repo_crawler/crawl_issues.py
+++ tools/repo_crawler/crawl_issues.py
@@ -17,23 +17,28 @@ _github_comments = github_comments.GitHubComments()
 
 def process_log(log, key, repo, dir_path):
     commit = re.match("(commit )(\w{10})", log).group(2)
-    file_prefix = dir_path + "/" + commit + "-"
+    file_prefix = commit + "-"
 
     m = re.findall(key + "-\d+", log)
     ids = Set(m)
     for i in ids:
-        file_path = file_prefix + i + ".xml"
-        if not os.path.isfile(file_path):
-            _jira_issue.download(i, file_path)
+        file_name = file_prefix + i + ".xml"
+        _jira_issue.download(i, dir_path, file_name)
     
-    m = re.findall("Closes #\d+", log)
+    m = re.findall("(?:[Cc]lose[ds]*|[Pp]ull[ \t]*[Rr]equest|[Mm]erge[ds]*)" \
+                   "[ \t]*#\d+",
+                   log)
     prs = Set(m)
     for pr in prs:
-        pr = pr[8:]
-        file_path = file_prefix + "GitHub-" + pr + ".xml"
-        if not os.path.isfile(file_path):
-            print "Downloading for", commit, "from GitHub:", repo, pr
-            _github_comments.download("apache", repo, pr, file_path)
+        pr = pr.split("#")[1]
+        file_path = os.path.join(dir_path,
+                                 file_prefix + "GitHub-" + pr + ".xml")
+        invalid_path = os.path.join(dir_path,
+                   ".invalid." + file_prefix + "GitHub-" + pr + ".xml")
+        if os.path.isfile(file_path) or os.path.isfile(invalid_path):
+            continue
+        print "Downloading for", commit, "from GitHub:", repo, pr
+        _github_comments.download("apache", repo, pr, file_path)
 
 def crawl_repo(repo_dir, key, tag, n):
     # Prepare the dir to store issues.
@@ -41,14 +46,14 @@ def crawl_repo(repo_dir, key, tag, n):
     path = repo + "-issues"
     if not os.path.isdir(path):
         os.mkdir(path)
-
-    git_repo = git.bake("-C", repo_dir)
+    git_repo = git.bake("-C", os.path.expanduser(repo_dir))
     git_repo.checkout(tag)
     for i in range(int(n)):
         try:
             log_str = str(git_repo.log("-1", "HEAD~" + str(i)))
         except Exception as e:
-            continue
+            print repo_dir, "ends at", i, "commits"
+            break
         process_log(log_str, key, repo, path)
 
 def main():
@@ -64,6 +69,8 @@ def main():
     conf_file = open(args.config_file)
     threads = []
     for line in conf_file:
+        if line.startswith("#"):
+            continue
         repo_args = tuple(line.split())
         t = threading.Thread(target=crawl_repo, args=line.split())
         threads.append(t)
--- tools/repo_crawler/jira_issue.py
+++ tools/repo_crawler/jira_issue.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
 import sys
 import urllib
 
@@ -14,10 +15,17 @@ class JiraIssue:
         self.url_prefix = url_prefix
         self.url_suffix = url_suffix
 
-    def download(self, issue_id, file_path):
+    def download(self, issue_id, dir_path, file_name):
         url = self.url_prefix + issue_id + "/" + issue_id + self.url_suffix
+        file_path = os.path.join(dir_path, file_name)
+        invalid_path = os.path.join(dir_path, ".invalid." + file_name)
+        if os.path.isfile(file_path) or os.path.isfile(invalid_path):
+            return
         try:
             print urllib.urlretrieve(url, file_path)[0]
+            if "Oops, you&#39;ve found a dead link" in open(file_path).read():
+                os.rename(file_path, invalid_path)
+                print "Invalid issue ID:", invalid_path
         except Exception as e:
             print "[Error] JiraIssue.download: ", type(e), e
 
