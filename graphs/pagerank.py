e7f83eeea2763da9739f8343390bb8e91157ab17
Hezheng Yin <hezheng@persper.org>
2017-04-03 18:25:15 -0700

    Add memory efficient PageRank using scipy.sparse
    The original PageRank implementation creates a dense transition matrix
    of size (num_nodes, num_nodes). When num_nodes = 10,000, this matrix
    takes 1GB of memory. However, the call graph of linux kernel v4.10 has
    approximately 470,000 nodes and 1816511 edges, which needs prohibitively
    huge memory. The new algorithm uses a different formula in order to
    keep transition matrix sparse throughout computation. Upon profiling,
    it is slightly slower than original implementation on graphs of moderate
    size, and still much faster than networkx's pagerank. The resulted
    PageRank vector is not identical to the original implementation,
    probably due to improvment on numerical stability.

--- graphs/pagerank.py
+++ graphs/pagerank.py
@@ -1,30 +1,37 @@
 from numpy import linalg as LA
 import numpy as np
+from scipy.sparse import coo_matrix
 
 def pagerank(G, alpha=0.85, epsilon=1e-5, max_iters=300):
+    """Memory efficient PageRank using scipy.sparse
+    This function implements Algo 1. in "A Survey on PageRank Computing"
+    """
     ni = {}
     for i, u in enumerate(G):
         ni[u] = i
         
     num_nodes = len(G.nodes())
-    P = np.zeros([num_nodes, num_nodes])
-    
+
+    row, col, data = [], [], []
     for u in G:
         num_out_edges = len(G[u])
-        if num_out_edges == 0:
-            P[:, ni[u]] = 1 / num_nodes
-        else:
+        if num_out_edges > 0:
+            w = 1 / num_out_edges
             for v in G[u]:
-                P[ni[v], ni[u]] = 1 / num_out_edges
-            
+                row.append(ni[v])
+                col.append(ni[u])
+                data.append(w)
+
+    P = coo_matrix((data, (row, col)), shape=(num_nodes, num_nodes)).tocsr()
     p = np.ones(num_nodes) / num_nodes
     v = np.ones(num_nodes) / num_nodes
         
     for i in range(max_iters):
-        new_v = alpha * np.dot(P, v) + (1 - alpha) * p
-        assert(new_v.shape == (num_nodes,))
-        delta = new_v - v
-        if LA.norm(delta) < epsilon:
+        new_v = alpha * P.dot(v)
+        gamma = LA.norm(v, 1) - LA.norm(new_v, 1)
+        new_v += gamma * p
+        delta = LA.norm(new_v - v, 1) 
+        if delta < epsilon:
             break
         v = new_v
         
