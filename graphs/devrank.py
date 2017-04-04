22f4d407d39dbe07f50d40fdfe3a53edb4dd4bb6
Hezheng Yin <hezheng@persper.org>
2017-04-03 18:34:16 -0700

    Add memory efficient DevRank using scipy.sparse

--- graphs/devrank.py
+++ graphs/devrank.py
@@ -1,45 +1,52 @@
 import numpy as np
 from numpy import linalg as LA
+from scipy.sparse import coo_matrix
 
 def devrank(G, count_self=False, alpha=0.85, epsilon=1e-5, max_iters=300):
+    """Memory efficient DevRank using scipy.sparse"""
     ni = {}
     for i, u in enumerate(G):
         ni[u] = i
         
-    sizes = {}
-    universe_size = 0
-    for u in G:
-        sizes[u] = G.node[u]['num_lines']
-        universe_size += sizes[u]
+    def sizeof(u):
+        return G.node[u]['num_lines']
         
     num_nodes = len(G.nodes())
-    P = np.zeros([num_nodes, num_nodes])
-    
+    row, col, data = [], [], []
     for u in G:
         num_out_edges = len(G[u])
-        if num_out_edges == 0:
-            P[:, ni[u]] = 1 / num_nodes
-        else:
+        if num_out_edges > 0:
             total_out_sizes = 0
             for v in G[u]:
-                total_out_sizes += sizes[v]
+                total_out_sizes += sizeof(v)
             if count_self:
-                total_out_sizes += sizes[u]
-                P[ni[u], ni[u]] = sizes[u] / total_out_sizes
+                total_out_sizes += sizeof(u)
+                row.append(ni[u])
+                col.append(ni[u])
+                data.append(sizeof(u) / total_out_sizes)
             for v in G[u]:
-                P[ni[v], ni[u]] = sizes[v] / total_out_sizes
+                row.append(ni[v])
+                col.append(ni[u])
+                data.append(sizeof(v) / total_out_sizes)
+
+    P = coo_matrix((data, (row, col)), shape=(num_nodes, num_nodes)).tocsr()
             
+    universe_size = 0
+    for u in G:
+        universe_size += sizeof(u)
+
     p = np.empty(num_nodes)
     for u in G:
-        p[ni[u]] = sizes[u] / universe_size
+        p[ni[u]] = sizeof(u) / universe_size
 
     v = np.ones(num_nodes) / num_nodes
         
     for i in range(max_iters):
-        new_v = alpha * np.dot(P, v) + (1 - alpha) * p
-        assert(new_v.shape == (num_nodes,))
-        delta = new_v - v
-        if LA.norm(delta) < epsilon:
+        new_v = alpha * P.dot(v)
+        gamma = LA.norm(v, 1) - LA.norm(new_v, 1)
+        new_v += gamma * p
+        delta = LA.norm(new_v - v, 1)
+        if delta < epsilon:
             break
         v = new_v
         
