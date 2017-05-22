cc8fe26cc864b6c616a12c5982ce8fe5f88a5d3b
Jinglei Ren <jinglei@persper.org>
2017-05-22 19:26:46 +0800

    [tools] Add a pair changes calculator

--- tools/excel_charts/distance.py
+++ tools/excel_charts/distance.py
@@ -10,3 +10,20 @@ def deviation(map1, map2, index):
         var += (values[index] - map2.get(func, values)[index])**2
     return math.sqrt(var / n)
 
+def pair_changes(map1, map2, index):
+    n = len(map1)
+    assert len(map2) == n
+    p = 0
+    keys = list(map1.keys())
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            d1 = map1[keys[i]][index] - map1[keys[j]][index]
+            d2 = map2[keys[i]][index] - map2[keys[j]][index]
+            if d1 == 0 and d2 == 0:
+                continue
+            elif d1 == 0 or d2 == 0:
+                p += 1
+            elif d1 * d2 < 0:
+                p += 1
+    return p
+
--- tools/excel_charts/tests/test_distance.py
+++ tools/excel_charts/tests/test_distance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import distance
+import math
 from statistics import mean
 from statistics import pstdev
 from random import random
@@ -11,14 +12,25 @@ class TestDistanceMethods(unittest.TestCase):
     def test_deviation(self):
         n = 1000000
         r = [random() for x in range(n)]
-        r = [1, 2, 3, 4]
         m = mean(r)
         map1 = {}
         map2 = {}
         for i, v in enumerate(r):
           map1[i] = [v]
           map2[i] = [m]
-        self.assertEqual(distance.deviation(map1, map2, 0), pstdev(r, m))
+        d1 = distance.deviation(map1, map2, 0)
+        d2 = pstdev(r, m)
+        self.assertTrue(math.isclose(d1, d2))
+
+    def test_pair_changes(self):
+        map1 = {'A': [1], 'B': [2], 'C': [3], 'D': [4], 'E': [5]}
+        map2 = {'A': [1], 'B': [3], 'C': [2], 'D': [4], 'E': [5]}
+        map3 = {'A': [3], 'B': [2], 'C': [1], 'D': [4], 'E': [5]}
+        map4 = {'A': [5], 'B': [1], 'C': [2], 'D': [3], 'E': [4]}
+        self.assertEqual(distance.pair_changes(map1, map1, 0), 0)
+        self.assertEqual(distance.pair_changes(map1, map2, 0), 1)
+        self.assertEqual(distance.pair_changes(map1, map3, 0), 3)
+        self.assertEqual(distance.pair_changes(map1, map4, 0), 4)
 
 if __name__ == '__main__':
     unittest.main()
