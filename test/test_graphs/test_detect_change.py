404598bec818cce324cbedbf075da3786d931b23
Hezheng Yin <hezheng@persper.org>
2017-05-22 14:50:07 -0700

    Add a test case for patch parser

--- test/test_graphs/test_detect_change.py
+++ test/test_graphs/test_detect_change.py
@@ -49,15 +49,29 @@ def test_detect_change():
         *func_ranges_result, *parsing_result))
 
 def test_patch_parser():
+    parser = PatchParser()
+
     patch2_truth = (
         [[0, 6]], 
         []
     )
-    parser = PatchParser()
     with open(os.path.join(dir_path, 'example2.patch'), 'r') as f:
         example2_patch = f.read()
         parsing_result = parser.parse(example2_patch)
         assert(parsing_result == patch2_truth)
+
+    # view patch3_truth here
+    # https://github.com/UltimateBeaver/test_feature_branch/commit/caaac10f604ea7ac759c2147df8fb2b588ee2a27
+    patch3_truth = (
+        [[10, 4], [12, 1], [14, 1], [17, 13]],
+        [[9, 10], [12, 12], [14, 14]] 
+    )
+    with open(os.path.join(dir_path, 'example3.patch'), 'r') as f:
+        example3_patch = f.read()
+        parsing_result = parser.parse(example3_patch)
+        print(parsing_result)
+        assert(parsing_result == patch3_truth)
+
 
 
 
