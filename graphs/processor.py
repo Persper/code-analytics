56bc99601399ca27a12228e0981e356289215b91
Hezheng Yin <hezheng@persper.org>
2017-07-10 19:28:33 -0700

    Serialize the hexsha instead of the Commit object

--- graphs/processor.py
+++ graphs/processor.py
@@ -7,14 +7,16 @@ print = functools.partial(print, flush=True)
 
 EMPTY_TREE_SHA = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
+
 def _diff_with_first_parent(commit):
-    if len(commit.parents) == 0: 
+    if len(commit.parents) == 0:
         prev_commit = EMPTY_TREE_SHA
     else:
         prev_commit = commit.parents[0]
     # commit.diff automatically detect renames
-    return commit.diff(prev_commit, 
-        create_patch=True, R=True, indent_heuristic=True)
+    return commit.diff(prev_commit,
+                       create_patch=True, R=True, indent_heuristic=True)
+
 
 def _fill_change_type(diff_index):
     for diff in diff_index:
@@ -22,19 +24,22 @@ def _fill_change_type(diff_index):
             diff.change_type = 'A'
         elif diff.deleted_file:
             diff.change_type = 'D'
-        elif diff.renamed: 
+        elif diff.renamed:
             diff.change_type = 'R'
-        elif (diff.a_blob and diff.b_blob and 
-             (diff.a_blob != diff.b_blob)):
+        elif (diff.a_blob and diff.b_blob and
+              (diff.a_blob != diff.b_blob)):
             diff.change_type = 'M'
         else:
             diff.change_type = 'U'
 
+
 def _print_diff_index(diff_index):
     print(" ".join([diff.change_type for diff in diff_index]))
 
+
 def _subject(msg):
-    return msg.split('\n', 1)[0].lstrip().rstrip() 
+    return msg.split('\n', 1)[0].lstrip().rstrip()
+
 
 class Processor():
 
@@ -45,34 +50,35 @@ class Processor():
         self.visited = set()
         self.last_processed_commit = None
 
-    def process(self, from_beginning=False, from_last_processed=False, 
-        num_commits=None, rev=None, into_branches=False, verbose=False, 
-        max_branch_length=100, end_commit_sha=None, checkpoint_interval=100):
+    def process(self, from_beginning=False, from_last_processed=False,
+                num_commits=None, rev=None, into_branches=False, verbose=False,
+                max_branch_length=100, end_commit_sha=None,
+                checkpoint_interval=100):
         """
         This function supports four ways of specifying the
-        range of commits to process: 
+        range of commits to process:
 
         Method 1: Pass `rev` parameter and set both
-            `from_beginning` and `from_last_processed` to False. 
+            `from_beginning` and `from_last_processed` to False.
             `rev` is the revision specifier which follows
             an extended SHA-1 syntax. Please refer to git-rev-parse
-            for viable options. `rev' should only include commits 
+            for viable options. `rev' should only include commits
             on the master branch.
 
-        Method 2: Set `from_beginning` to True and 
+        Method 2: Set `from_beginning` to True and
             pass `num_commits` parameter. Using this
             method, the function will start from the
             very first commit on the master branch and
             process the following `num_commits` commits
             (also on the master branch).
 
-        Method 3: Set `from_last_processed` to True and pass 
-            `num_commits` parameter. Using this method, the 
-            function will resume processing from succeeding commit of 
-            `self.last_processed_commit` for `num_commits` commits. 
+        Method 3: Set `from_last_processed` to True and pass
+            `num_commits` parameter. Using this method, the
+            function will resume processing from succeeding commit of
+            `self.last_processed_commit` for `num_commits` commits.
 
-        Method 4: Set `from_last_processed` to True and pass 
-            `end_commit_sha` parameter. The range of continued processing 
+        Method 4: Set `from_last_processed` to True and pass
+            `end_commit_sha` parameter. The range of continued processing
             will be `self.last_processed_commit.hexsha..end_commit_sha`.
 
         Args:
@@ -81,19 +87,19 @@ class Processor():
             from_beginning: A boolean flag, see above.
             continue: A boolean flag, see above.
             into_branches: A boolean flag, if True, the process function
-                will operate in two phases. 
+                will operate in two phases.
 
-                In the first phase, a call commit graph is contructed 
-                by traversing the specified range of commits on the master 
-                branch. Merge commits are detected and recorded if the 
-                start commit (on master) and end/merge commit of the 
+                In the first phase, a call commit graph is contructed
+                by traversing the specified range of commits on the master
+                branch. Merge commits are detected and recorded if the
+                start commit (on master) and end/merge commit of the
                 corresponding branch are both within the range of
-                traversal. Those recorded merge commits do not 
-                get any credits (thus they are not present in 
+                traversal. Those recorded merge commits do not
+                get any credits (thus they are not present in
                 self.history data structure).
 
                 In the second phase, it traverses all the branches detected
-                in the first phase and assign them due credits.   
+                in the first phase and assign them due credits.
 
             max_branch_length: An int, the maximum number of commits
                 to trace back before abortion.
@@ -107,7 +113,7 @@ class Processor():
 
         # Method 2
         if from_beginning:
-            if num_commits == None:
+            if num_commits is None:
                 num_commits = 0
             self.commits = list(
                 self.repo.iter_commits(first_parent=True))[-num_commits:]
@@ -119,8 +125,7 @@ class Processor():
 
             # Method 4
             if end_commit_sha:
-                rev = (self.last_processed_commit.hexsha + '..'
-                       + end_commit_sha)
+                rev = self.last_processed_commit.hexsha + '..' + end_commit_sha
                 self.commits = list(self.repo.iter_commits(
                     rev, first_parent=True))
             # Method 3
@@ -129,7 +134,7 @@ class Processor():
                 self.commits = list(self.repo.iter_commits(
                     rev, first_parent=True))[-num_commits:]
             else:
-                print("Both end_commit_sha and num_commits are None, terminated.")
+                print("Both end_commit_sha and num_commits are None.")
                 return
 
         else:
@@ -174,8 +179,9 @@ class Processor():
                 _print_diff_index(diff_index)
 
             if into_branches:
-                is_merge_commit = self._detect_branch(commit, max_branch_length)
-            else: 
+                is_merge_commit = self._detect_branch(
+                    commit, max_branch_length)
+            else:
                 is_merge_commit = False
 
             for diff in diff_index:
@@ -184,7 +190,7 @@ class Processor():
                     continue
 
                 if diff.change_type == 'A':
-                    self.on_add(diff, commit, is_merge_commit) 
+                    self.on_add(diff, commit, is_merge_commit)
 
                 elif diff.change_type == 'D':
                     self.on_delete(diff, commit, is_merge_commit)
@@ -197,11 +203,10 @@ class Processor():
 
             counter += 1
 
-
         # 2nd phase
         if into_branches:
 
-            counter = 1 
+            counter = 1
             branch_cnt = 1
             start = time.time()
 
@@ -213,7 +218,8 @@ class Processor():
                     if verbose:
                         print('------ Commit No.{} '.format(counter),
                               'Branch No.{} {} {}------'.format(
-                                branch_cnt, commit.hexsha, _subject(commit.message)))
+                                branch_cnt, commit.hexsha,
+                                _subject(commit.message)))
                     else:
                         print('------ Commit No.{} '.format(counter),
                               'Branch No.{} {}------'.format(
@@ -223,8 +229,10 @@ class Processor():
                             time.time() - start))
 
                     if counter % checkpoint_interval == 0:
-                        repo_name = os.path.basename(self.repo_path.rstrip('/'))
-                        self.save(repo_name + '-2nd-' + str(counter) + '.pickle')
+                        repo_name = os.path.basename(
+                            self.repo_path.rstrip('/'))
+                        self.save(
+                            repo_name + '-2nd-' + str(counter) + '.pickle')
 
                     # generate diff_index by diff commit with its first parent
                     diff_index = _diff_with_first_parent(commit)
@@ -241,7 +249,7 @@ class Processor():
                             continue
 
                         if diff.change_type == 'A':
-                            self.on_add2(diff, commit) 
+                            self.on_add2(diff, commit)
 
                         elif diff.change_type == 'D':
                             self.on_delete2(diff, commit)
@@ -266,28 +274,28 @@ class Processor():
         pass
 
     def on_add(self, diff, commit, is_merge_commit):
-        return 0 
+        return 0
 
     def on_delete(self, diff, commit, is_merge_commit):
-        return 0 
+        return 0
 
     def on_rename(self, diff, commit, is_merge_commit):
-        return 0 
+        return 0
 
     def on_modify(self, diff, commit, is_merge_commit):
-        return 0 
+        return 0
 
     def on_add2(self, diff, commit):
-        return 0 
+        return 0
 
     def on_delete2(self, diff, commit):
-        return 0 
+        return 0
 
     def on_rename2(self, diff, commit):
-        return 0 
+        return 0
 
     def on_modify2(self, diff, commit):
-        return 0 
+        return 0
 
     def _detect_branch(self, commit, max_branch_length):
         found = False
@@ -297,9 +305,9 @@ class Processor():
             branch = [cur_commit]
 
             while(branch_length <= max_branch_length):
-                prev_commit = cur_commit.parents[0]  
-                if (prev_commit.hexsha in self.visited or 
-                    len(prev_commit.parents) == 0):
+                prev_commit = cur_commit.parents[0]
+                if (prev_commit.hexsha in self.visited or
+                   len(prev_commit.parents) == 0):
                     found = True
                     break
                 branch.append(prev_commit)
@@ -314,7 +322,7 @@ class Processor():
         state = {
             'visited': self.visited,
             'repo_path': self.repo_path,
-            'last_processed_commit': self.last_processed_commit
+            'last_sha': self.last_processed_commit.hexsha
         }
         return state
 
@@ -322,6 +330,7 @@ class Processor():
         self.__dict__.update(state)
         self.repo = initialize_repo(state['repo_path'])
         self.git = self.repo.git
+        self.last_processed_commit = self.repo.commit(state['last_sha'])
 
     def save(self, fname):
         with open(fname, 'wb+') as f:
