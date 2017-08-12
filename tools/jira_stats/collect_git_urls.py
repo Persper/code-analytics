9c2c52e7b6e468f64d7669d76c53e5e6afda214e
Jinglei Ren <jinglei@persper.org>
2017-08-12 20:44:29 +0800

    [tools] Collect GitHub repo URLs of Apache projects

---
+++ tools/jira_stats/collect_git_urls.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import re
+import requests
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Collect Apache and GitHub repo URLs of Apache projects')
+    parser.add_argument('-f', '--file', required=True,
+                        help='the output file')
+    args = parser.parse_args()
+
+    if os.path.isfile(args.file):
+        sys.exit('Error: output file already exists!')
+
+    out_file = open(args.file, 'w')
+
+    APACHE_GIT = 'https://git.apache.org/'
+
+    resp = requests.get(APACHE_GIT)
+
+    pattern = re.compile(r'<td>(.+)</td>\s*'
+                     r'<td>\s*<a href="(.+)">.+</a>\s*</td>\s*'
+                     r'<td>\s*<a href="(https\://github\.com/.+)">')
+
+    for match in pattern.finditer(resp.text):
+        name = match.group(1)
+        apache_repo = match.group(2)
+        github_repo = match.group(3)
+        print(name, apache_repo, github_repo, sep=',', file=out_file)
+
+    out_file.close()
+
+if __name__ == '__main__':
+    main()
+
