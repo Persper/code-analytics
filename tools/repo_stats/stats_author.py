9551caae5e84ef30a63e3ef2ea902bf072c5f2ac
Jinglei Ren <jinglei@persper.org>
2017-09-09 18:30:54 +0800

    [tools] Revise stats_author.py
    Parse commit stats in python.
    Add functionality that counts authors of projects.

--- tools/repo_stats/stats_author.py
+++ tools/repo_stats/stats_author.py
@@ -2,37 +2,56 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from sh import awk
-from sh import git
+from sh.contrib import git
 import os
 import subprocess
 import sys
 
-
-def stats(repo_dir):
-    print(repo_dir + ":")
-    print("\tTop authors [author] [# commits]")
-    git_cmd = ["git", "-C", repo_dir, "shortlog", "-sn"]
-    commits_per_author = subprocess.Popen(git_cmd, stdout=subprocess.PIPE)
-    awk_script = '''
-    BEGIN { count_commits = 0 }
-    { count_commits += $1; if (NR <= 10) print "\t" $2 "\t" $1 }
-    END { print "# total commits: " count_commits }
-    '''
-    print(awk(awk_script, _in=commits_per_author.stdout))
+def stats_commits(repo_path, author_stats=None):
+    if not author_stats:
+        author_stats = { }
+    git_cmd = ['git', '--no-pager', '-C', repo_path, 'shortlog', '-sn']
+    p = subprocess.Popen(git_cmd, stdout=subprocess.PIPE)
+    with os.fdopen(os.dup(p.stdout.fileno())) as commits_per_author:
+        for line in commits_per_author:
+            num, name = [s.strip() for s in line.split('\t')]
+            if not name in author_stats:
+                author_stats[name] = { 'n_commits': int(num) }
+            else:
+                author_stats[name]['n_commits'] = int(num)
+    return author_stats
 
 def main():
     parser = argparse.ArgumentParser(
-        description='List author stats of git repos')
-    parser.add_argument('dir', nargs='+', help='Dir of a git repo')
+        description='List author stats of git repo(s)')
+    parser.add_argument('-c', '--count-commits', metavar='DIR',
+                        help='Git repo dir to list authors and their # commits')
+    parser.add_argument('-a', '--count-authors', metavar='DIR', nargs='+',
+                        help='Multiple git repos to list their # authors')
     args = parser.parse_args()
+    if args.count_commits:
+        if not os.path.isdir(args.count_commits):
+            sys.exit('Error: ' + args.dir + ' is not a valid dir!')
+        author_stats = stats_commits(args.count_commits)
+        for name, stats in sorted(author_stats.items(),
+                                  key=lambda x: x[1]['n_commits'],
+                                  reverse=True):
+            print(name, stats['n_commits'], sep=',')
+    elif args.count_authors:
+        project_authors = { }
+        for d in args.count_authors:
+            if os.path.isfile(d) or d.startswith('.'):
+                continue
+            repo_name = os.path.basename(os.path.normpath(d))
+            print('Parsing ' + repo_name)
+            project_authors[repo_name] = stats_commits(d)
+        for repo_name, author_stats in sorted(project_authors.items(),
+                                            key=lambda x: len(x[1]),
+                                            reverse=True):
+            print(repo_name, len(author_stats), sep=',')
+    else:
+        sys.exit('Error: see -h for usage.')
 
-    for repo_dir in args.dir:
-        if os.path.isdir(repo_dir):
-            stats(repo_dir)
-        else:
-            print('Error: ' + repo_dir + ' is not a valid dir!')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
