4f1611162361096566839876d34130553331417f
Jinglei Ren <jinglei@persper.org>
2017-12-09 16:43:05 +0800

    [tools] Count commits in stats_commit.py

--- tools/repo_stats/stats_commit.py
+++ tools/repo_stats/stats_commit.py
@@ -5,6 +5,7 @@ import git
 import json
 import re
 
+
 def main():
     parser = argparse.ArgumentParser(
         description='List commit stats of a git repo')
@@ -12,23 +13,48 @@ def main():
                         help='Dir of the repo to analyze')
     parser.add_argument('-b', '--branch', default='master',
                         help='Branch of the repo to analyze')
-    parser.add_argument('-o', '--output-file', required=True,
+    parser.add_argument('-o', '--output-file',
                         help='Output JSON file')
+    parser.add_argument('-s', '--show-stats', action='store_true',
+                        help='Show stats of commits instead of outputting')
     args = parser.parse_args()
 
     repo = git.Repo(args.repo_dir)
-    outfile = open(args.output_file, 'w')
 
+    if args.show_stats:
+        email2stats = {}
+        for i, commit in enumerate(repo.iter_commits(args.branch)):
+            if len(commit.parents) > 1:
+                continue
+            email = commit.author.email
+            if email not in email2stats:
+                email2stats[email] = {'commits': 1, 'begin': i, 'end': i,
+                                      'names': [commit.author.name]}
+            else:
+                stats = email2stats[email]
+                stats['commits'] += 1
+                stats['end'] = i
+                if commit.author.name not in stats['names']:
+                    stats['names'].append(commit.author.name)
+
+        for email, stats in email2stats.items():
+            print('%s\t%d\t%d\t%d\t' % (email, stats['commits'],
+                                        stats['begin'], stats['end']),
+                  stats['names'])
+        return
+
+    outfile = open(args.output_file, 'w')
     commit_list = []
 
     user_name, repo_name = re.split('[/:.]', repo.remotes.origin.url)[-3:-1]
     url_base = 'https://github.com/%s/%s/commit/' % (user_name, repo_name)
 
     for commit in repo.iter_commits(args.branch):
-        if len(commit.parents) > 1: continue
+        if len(commit.parents) > 1:
+            continue
         n_add = 0
         n_del = 0
-        files = [ ]
+        files = []
         for path, stat in commit.stats.files.items():
             files.append(path)
             n_add += stat['insertions']
@@ -46,6 +72,7 @@ def main():
 
     json.dump(commit_list, outfile)
     outfile.close()
+
 
 if __name__ == '__main__':
     main()
