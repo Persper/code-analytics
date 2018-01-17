69deaeea4fb721f5a38c033fcc7c91f58a357392
Hezheng Yin <hezheng@persper.org>
2018-01-17 10:34:33 +0800

    Move diff helper into git_tools.py

--- persper/graphs/git_tools.py
+++ persper/graphs/git_tools.py
@@ -2,6 +2,19 @@ from git.exc import InvalidGitRepositoryError, NoSuchPathError
 from git import Repo
 import sys
 
+EMPTY_TREE_SHA = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+
+
+def _diff_with_first_parent(commit):
+    if len(commit.parents) == 0:
+        prev_commit = EMPTY_TREE_SHA
+    else:
+        prev_commit = commit.parents[0]
+    # commit.diff automatically detect renames
+    return commit.diff(prev_commit,
+                       create_patch=True, R=True, indent_heuristic=True)
+
+
 def initialize_repo(repo_path):
     try:
         repo = Repo(repo_path)
@@ -12,6 +25,7 @@ def initialize_repo(repo_path):
         print("No such path error!")
         sys.exit(-1)
     return repo
+
 
 def get_contents(repo, commit, path):
     """Get contents of a path within a specific commit"""
