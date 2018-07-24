90454c7b998e54c3a6e08135cd901f50a706df47
Hezheng Yin <hezheng@persper.org>
2018-07-24 12:14:11 -0700

    Minor formatting

--- persper/graphs/pagerank.py
+++ persper/graphs/pagerank.py
@@ -2,6 +2,7 @@ from numpy import linalg as LA
 import numpy as np
 from scipy.sparse import coo_matrix
 
+
 def pagerank(G, alpha=0.85, epsilon=1e-5, max_iters=300):
     """Memory efficient PageRank using scipy.sparse
     This function implements Algo 1. in "A Survey on PageRank Computing"
@@ -9,7 +10,7 @@ def pagerank(G, alpha=0.85, epsilon=1e-5, max_iters=300):
     ni = {}
     for i, u in enumerate(G):
         ni[u] = i
-        
+
     num_nodes = len(G.nodes())
 
     row, col, data = [], [], []
@@ -25,18 +26,18 @@ def pagerank(G, alpha=0.85, epsilon=1e-5, max_iters=300):
     P = coo_matrix((data, (row, col)), shape=(num_nodes, num_nodes)).tocsr()
     p = np.ones(num_nodes) / num_nodes
     v = np.ones(num_nodes) / num_nodes
-        
+
     for i in range(max_iters):
         new_v = alpha * P.dot(v)
         gamma = LA.norm(v, 1) - LA.norm(new_v, 1)
         new_v += gamma * p
-        delta = LA.norm(new_v - v, 1) 
+        delta = LA.norm(new_v - v, 1)
         if delta < epsilon:
             break
         v = new_v
-        
+
     pr = {}
     for u in G:
         pr[u] = v[ni[u]]
-    
+
     return pr
