865ad933c59f6385863a708f0fbb390b633ca148
Jinglei Ren <jinglei@persper.org>
2018-09-07 01:26:49 +0800

    Provide a default HTTP implementation of GraphServer

---
+++ persper/graphs/graph_server_http.py
@@ -0,0 +1,48 @@
+from networkx.readwrite import json_graph
+from persper.graphs.graph_server import GraphServer
+import re
+import requests
+import urllib.parse
+
+
+class GraphServerHttp(GraphServer):
+    def __init__(self, server_addr, filename_regex_strs):
+        self.server_addr = server_addr
+        self.filename_regexes = [re.compile(regex_str) for regex_str in filename_regex_strs]
+
+    def update_graph(self, old_filename, old_src, new_filename, new_src, patch):
+        payload = {'oldFname': old_filename,
+                   'oldSrc': old_src,
+                   'newFname': new_filename,
+                   'newSrc': new_src,
+                   'patch': patch.decode('utf-8', 'replace')}
+
+        update_url = urllib.parse.urljoin(self.server_addr, '/update')
+        r = requests.post(update_url, json=payload).json()
+        return r['idToLines'], r['idMap']
+
+    def parse(self, old_filename, old_src, new_filename, new_src, patch):
+        payload = {'oldFname': old_filename,
+                   'oldSrc': old_src,
+                   'newFname': new_filename,
+                   'newSrc': new_src,
+                   'patch': patch.decode('utf-8', 'replace')}
+
+        stats_url = urllib.parse.urljoin(self.server_addr, '/stats')
+        r = requests.get(stats_url, json=payload).json()
+        return r['idToLines'], r['idMap']
+
+    def get_graph(self):
+        graph_url = self.server_addr + '/callgraph'
+        r = requests.get(graph_url)
+        return json_graph.node_link_graph(r.json())
+
+    def reset_graph(self):
+        reset_url = urllib.parse.urljoin(self.server_addr, '/reset')
+        requests.post(reset_url)
+
+    def filter_file(self, filename):
+        for regex in self.filename_regexes:
+            if not regex.match(filename):
+                return False
+        return True
