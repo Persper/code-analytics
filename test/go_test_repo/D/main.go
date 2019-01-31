ae5f41ffba2ed1e7cd0a3e801d76d8764db77066
xiaoerlaigeid <302807330@qq.com>
2019-02-01 00:54:10 +0800

    add go-test-repo

---
+++ test/go_test_repo/D/main.go
@@ -0,0 +1,40 @@
+package main
+import(
+	"fmt"
+	"math"
+)
+type a func()
+type b func()
+type c func()
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) sum() float64{
+	s := v.X+v.Y
+	fmt.Println(s)
+	return s
+
+}
+func (v Vertex) Abs() float64 {
+	funcA()
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func funcA () {
+	fmt.Println("func A is called!");
+}
+func funcB () {
+	fmt.Println("func B is called!");
+}
+func main() {
+	a := funcA
+	b := funcB
+	c := a
+	b()
+	c()
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	v.sum()
+}
+ 
