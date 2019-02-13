77e2911a4f2967af28488afff55c92f7af6e35e9
Hezheng Yin <hezheng@meri.co>
2019-02-12 17:26:22 -0800

    Fix various bugs in test_analyzer_go.py

--- test/test_analytics/test_analyzer_go.py
+++ test/test_analytics/test_analyzer_go.py
@@ -1,22 +1,17 @@
 import os
 import time
-#import pytest
+import pytest
 import subprocess
-#from persper.analytics.graph_server import JS_FILENAME_REGEXES
-from persper.analytics.graph_server_http_go import GraphServerHttp
+from persper.analytics.graph_server import GO_FILENAME_REGEXES
+from persper.analytics.go import GoGraphServer
 from persper.analytics.analyzer import Analyzer
 from persper.util.path import root_path
-#from .util import assert_size_match_history
-GO_FILENAME_REGEXES = [
-    r'.+\.go$',
-    r'^(?!src/).+'
-]
-# Use a port other than the default 8080 in case of collision
+
+# TODO: Use a port other than the default 8080 in case of collision
 server_port = 8080
-server_path = os.path.join(root_path, 'contribs/go-callgraph/graphserver-linux-amd64')
 
 
-# @pytest.fixture(scope='module')
+@pytest.fixture(scope='module')
 def az():
     """ Build the test repo if not already exists
 
@@ -34,56 +29,54 @@ def az():
         cmd = '{} {}'.format(script_path, test_src_path)
         subprocess.call(cmd, shell=True)
 
-    return Analyzer(repo_path, GraphServerHttp(server_addr, GO_FILENAME_REGEXES))
+    return Analyzer(repo_path, GoGraphServer(server_addr, GO_FILENAME_REGEXES))
 
 
-def test_az(az: Analyzer):
-    # my_env = os.environ.copy()
-    # my_env["PORT"] = str(server_port)
-    # p = subprocess.Popen(['node', server_path], env=my_env)
-    # p = subprocess.Popen(["./"+server_path],env=my_env)
-    print("server_path",server_path)
-    #p = subprocess.call(server_path,shell = True)
-    try:
-        # wait for the server to spin up
-        time.sleep(1.0)
-        az._graph_server.reset_graph()
-        az.analyze()
-        ccgraph = az.get_graph()
-        print("try")
-        print("ccgraph")
-        # history_truth = {
-        #     'C': {'main.js:funcB:9:12': 1,
-        #           'main.js:global': 1,
-        #           'main.js:main:7:16': 1},
-        #     'B': {'main.js:funcB:9:11': 3,
-        #           'main.js:global': 7,
-        #           'main.js:main:7:15': 7},
-        #     'A': {'main.js:funcA:3:5': 3,
-        #           'main.js:main:7:10': 4,
-        #           'main.js:global': 12}
-        # }
-        print(ccgraph)
-        commits = ccgraph.commits()
-        print(commits)
-        for func, data in ccgraph.nodes(data=True):
-            history = data['history']
-            for cindex, csize in history.items():
-                commit_message = commits[int(cindex)]['message']
-                # assert(csize == history_truth[commit_message.strip()][func])
-                print(csize)
+def test_analzyer_go(az):
+    az._graph_server.reset_graph()
+    az.analyze()
+    ccgraph = az.get_graph()
 
-        # edges_truth = [
-        #     ('main.js:funcB:9:12', 'Native:Window_prototype_print'),
-        #     ('main.js:funcB:9:12', 'main.js:funcA:3:5'),
-        #     ('main.js:funcA:3:5', 'Native:Window_prototype_print'),
-        #     ('main.js:main:7:16', 'main.js:funcB:9:12'),
-        #     ('main.js:main:7:16', 'main.js:funcA:3:5'),
-        #     ('main.js:global', 'main.js:main:7:16')
-        # ]
-        # assert(set(az.graph_server.get_graph().edges()) == set(edges_truth))
-        print(set(az._graph_server.get_graph().edges() ) )
-    finally:
-        print("over")
-#        p.terminate()
-test_az(az())
+    history_truth = {
+        'C': {'Abs': 5,
+              'funcA': 0,
+              'funcB': 1,
+              'main': 0},
+        'B': {'Abs': 3,
+              'funcA': 0,
+              'funcB': 3,
+              'main': 5},
+        'A': {'Abs': 3,
+              'funcA': 3,
+              'main': 6}
+    }
+
+    commits = ccgraph.commits()
+    for func, data in ccgraph.nodes(data=True):
+        history = data['history']
+        for cindex, csize in history.items():
+            commit_message = commits[int(cindex)]['message']
+            assert(csize == history_truth[commit_message.strip()][func])
+
+    edges_added_by_A = set([
+        ('Abs', 'Sqrt'),
+        ('funcA', 'Println'),
+        ('main', 'a'),
+        ('main', 'Println'),
+        ('main', 'Abs'),
+    ])
+
+    edges_added_by_B = set([
+        ('Abs', 'funcA'),
+        ('funcB', 'Println'),
+        ('main', 'b'),
+        ('main', 'c'),
+    ])
+
+    edges_added_by_C = set([
+        ('Abs', 'a'),
+        ('funcB', 'funcA')
+    ])
+
+    all_edges = edges_added_by_A.union(edges_added_by_B).union(edges_added_by_C)
+    assert(set(az._graph_server.get_graph().edges()) == all_edges)
