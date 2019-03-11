e87f8eec82d96a0086fbd909bb91239955cc0294
xinyan <xinyan@meri.co>
2019-03-11 23:46:46 +0800

    Code cleanup.

--- persper/analytics/analyzer2.py
+++ persper/analytics/analyzer2.py
@@ -1,13 +1,13 @@
 import asyncio
-from abc import ABC
 import collections.abc
-from typing import Union, Set, Optional, List
+from abc import ABC
+from typing import List, Optional, Set, Union
 
 from git import Commit, Diff, DiffIndex, Repo
 
-from persper.analytics.git_tools import (diff_with_commit, get_contents)
-from persper.analytics.graph_server import CommitSeekingMode, GraphServer
 from persper.analytics.commit_classifier import CommitClassifier
+from persper.analytics.git_tools import diff_with_commit, get_contents
+from persper.analytics.graph_server import CommitSeekingMode, GraphServer
 from persper.analytics.score import commit_overall_scores
 
 
