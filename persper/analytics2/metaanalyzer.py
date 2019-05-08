ba46358d734babde2ef822b4e09902f34b312c0c
xinyan <xinyan@meri.co>
2019-05-08 23:54:10 +0800

    Remove MetaAnalyzer.first_parent_only because we actually cannot control this.

--- persper/analytics2/metaanalyzer.py
+++ persper/analytics2/metaanalyzer.py
@@ -7,11 +7,13 @@ from persper.analytics2.abstractions.repository import ICommitRepository
 class MetaAnalyzer():
     """
     Coordinates `ICommitAnalyzer` and `IPostAnalyzer` implementation, doing analysis through the commit history.
+    params
+        origin_commit, terminal_commit: See `ICommitRepository.enum_commits` for details.
     """
+
     def __init__(self, history_provider: ICommitRepository,
                  commit_analyzers: Iterable[ICommitAnalyzer], post_analyzers: Iterable[IPostAnalyzer],
                  origin_commit: str = None, terminal_commit: str = "HEAD",
-                 first_parent_only: bool = False,
                  analyzed_commits: Iterable[str] = None):
         if not isinstance(history_provider, ICommitRepository):
             raise ValueError("Expect ICommitRepository instance for history_provider.")
@@ -21,7 +23,6 @@ class MetaAnalyzer():
         self._post_analyzers = list(post_analyzers)
         self._origin_commit = origin_commit
         self._terminal_commit = terminal_commit
-        self._first_parent_only = first_parent_only
         self._analyzed_commits = set(analyzed_commits) if analyzed_commits else set()
 
     @property
@@ -39,14 +40,6 @@ class MetaAnalyzer():
     @terminal_commit.setter
     def terminal_commit(self, value: str):
         self._terminal_commit = value
-
-    @property
-    def first_parent_only(self):
-        return self._first_parent_only
-
-    @first_parent_only.setter
-    def first_parent_only(self, value: bool):
-        self._first_parent_only = value
 
     def analyze(self, max_commits: int = 100):
         pass
