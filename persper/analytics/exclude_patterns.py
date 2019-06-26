f3ce79e5300894782a33f4e5b96c7c5626209376
Hezheng Yin <hezheng@meri.co>
2019-06-26 16:17:58 -0700

    Add EXCLUDE_PATTERNS

---
+++ persper/analytics/exclude_patterns.py
@@ -0,0 +1,17 @@
+
+EXCLUDE_PATTERNS = [
+    r'^(.+/)*dist/',
+    r'^(.+/)*test(s)?/',
+    r'^(.+/)*spec/',
+    r'^(.+/)*build/',
+    r'^(.+/)*bin/',
+    r'^(.+/)*doc(s)?/',
+    r'^(.+/)*example(s)?/',
+    r'^(.+/)*config/',
+    r'^(.+/)*node_modules/',
+    r'^(.+/)*vendor/',
+    r'^(.+/)*target/',
+    r'^(.+/)*gen/',
+    r'^(.+/)*generated/',
+    r'^Godeps/',
+]
