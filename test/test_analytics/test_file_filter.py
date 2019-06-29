155326f86a70fe13b76d75fb41049ea8ec361fde
Hezheng Yin <hezheng@meri.co>
2019-06-29 15:55:41 -0700

    Minor change to resolve pep8 warning

--- test/test_analytics/test_file_filter.py
+++ test/test_analytics/test_file_filter.py
@@ -5,12 +5,12 @@ from persper.analytics.cpp import CPPGraphServer
 
 def test_file_filter():
     gs = CGraphServer()
-    assert gs.filter_file('io.cpp') == False
-    assert gs.filter_file('random.c') == True
-    assert gs.filter_file('src/random_dir/random.h') == True
+    assert gs.filter_file('io.cpp') is False
+    assert gs.filter_file('random.c') is True
+    assert gs.filter_file('src/random_dir/random.h') is True
 
     gs2 = CPPGraphServer()
-    assert gs2.filter_file('io.cpp') == True
-    assert gs2.filter_file('random.c') == True
-    assert gs2.filter_file('src/random_dir/random.h') == True
-    assert gs2.filter_file('random.js') == False
+    assert gs2.filter_file('io.cpp') is True
+    assert gs2.filter_file('random.c') is True
+    assert gs2.filter_file('src/random_dir/random.h') is True
+    assert gs2.filter_file('random.js') is False
