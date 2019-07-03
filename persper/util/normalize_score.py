9bcda7ba8a0a6e098eee523b35ffe75ab10080d4
doubleqiang <qiang.sun@meri.co>
2019-07-03 23:56:30 +0800

    add catch ZeroDivisionError in normalize_with_coef

--- persper/util/normalize_score.py
+++ persper/util/normalize_score.py
@@ -1,5 +1,5 @@
 from typing import Dict
-
+import logging
 
 def normalize_with_coef(scores: Dict[str, float], coef=1.0) -> Dict[str, float]:
     normalized_scores = {}
@@ -8,6 +8,10 @@ def normalize_with_coef(scores: Dict[str, float], coef=1.0) -> Dict[str, float]:
         score_sum += score
 
     for idx in scores:
-        normalized_scores[idx] = scores[idx] / score_sum * coef
+        try:
+            normalized_scores[idx] = scores[idx] / score_sum * coef
+        except ZeroDivisionError:
+            normalized_scores[idx] = 0
+            logging.error('discover ZeroDivisionError!')
 
     return normalized_scores
