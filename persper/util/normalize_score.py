8e408c805568f5797963b58c24ea790bc1056268
Hezheng Yin <hezheng@meri.co>
2019-07-30 15:08:11 -0700

    Minor refactor for normalize_with_coef

--- persper/util/normalize_score.py
+++ persper/util/normalize_score.py
@@ -1,17 +1,19 @@
-from typing import Dict
 import logging
+from typing import Dict
+
 
 def normalize_with_coef(scores: Dict[str, float], coef=1.0) -> Dict[str, float]:
+    """Normalize the values of scores to sum to coef (whose default is 1)"""
     normalized_scores = {}
-    score_sum = 0
+    score_sum = 0.0
     for _, score in scores.items():
         score_sum += score
 
-    for idx in scores:
-        try:
-            normalized_scores[idx] = scores[idx] / score_sum * coef
-        except ZeroDivisionError:
-            normalized_scores[idx] = 0
-            logging.error('discover ZeroDivisionError!')
-
+    if score_sum == 0:
+        logging.error('normalize_with_coef: ZeroDivisionError!')
+        for k in scores:
+            normalized_scores[k] = 0.0
+    else:
+        for k, v in scores.items():
+            normalized_scores[k] = v / score_sum * coef
     return normalized_scores
